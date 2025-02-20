d10fe4e3bafa99d0b52ec879f08282bec537e0a2
amirulkanak <coder.kanak@gmail.com>
2025-02-21 00:01:37 +0600

    create api fetchtask, add, update, delete task with axios

---
+++ client/src/api/taskService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// backend Api URL
+const API_URL = import.meta.env.VITE_BACKEND_API_URL;
+
+export const fetchTasks = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const addTask = async (task) => {
+  const response = await axios.post(API_URL, task);
+  return response.data;
+};
+
+export const updateTask = async (taskId, updatedData) => {
+  await axios.put(`${API_URL}/${taskId}`, updatedData);
+};
+
+export const deleteTask = async (taskId) => {
+  await axios.delete(`${API_URL}/${taskId}`);
+};
