3ebfe92d068bcf3ebdb16bbb2af7bef90607216b
amirulkanak <coder.kanak@gmail.com>
2025-02-22 00:06:44 +0600

    add due date and update taskSchema

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 
-const taskSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  category: String,
+const TaskSchema = new mongoose.Schema({
+  title: { type: String, required: true, maxlength: 50 },
+  description: { type: String, maxlength: 200 },
+  category: {
+    type: String,
+    enum: ['To-Do', 'In Progress', 'Done'],
+    required: true,
+  },
   timestamp: { type: Date, default: Date.now },
+  dueDate: { type: Date },
 });
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model('Task', TaskSchema);
 
 export default Task;
